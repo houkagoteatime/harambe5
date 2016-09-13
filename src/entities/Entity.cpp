5e524022ec24c33d9460be210d47bf52f27ebebf
J0hnCena <joshua99wong@gmail.com>
2016-09-13 10:31:58 -0500

    Create entity and Player classes

---
+++ src/entities/Entity.cpp
@@ -0,0 +1,6 @@
+#include "Entity.h"
+
+Entity::Entity(const std::string& mediaPath, irr::core::vector3df position, irr::core::vector3df rotation) : path(mediaPath), pos(position), rot(rotation)
+{
+
+}
