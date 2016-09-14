ec9ce0b9f3ef33397a4ad0db62b7e27a0d6e9a46
Harvey <1haveygao@gmail.com>
2016-09-14 23:30:21 +0900

    Collision without collisions

--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -1,6 +1,10 @@
 #include "Entity.h"
 
-Entity::Entity(const std::string& mediaPath, irr::core::vector3df position, irr::core::vector3df rotation) : path(mediaPath), pos(position), rot(rotation)
+Entity::Entity(const std::string& mediaPath, 
+	       irr::core::vector3df position, 
+	       irr::core::vector3df rotation) : path(mediaPath), 
+	       pos(position), 
+	       rot(rotation)
 {
 
 }
